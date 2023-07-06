2e52b72b07e67abb45ce96d4a4b299abeb1e2bd0
Nguyen Dinh Thong <nguyenthong244466666@gmail.com>
2023-07-06 22:49:11 +0700

    set up url backend

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
-export const URL_BACKEND = 'http://localhost:8000';
-// export const URL_BACKEND = 'https://flower-shop-be.vercel.app';
+// export const URL_BACKEND = 'http://localhost:8000';
+export const URL_BACKEND = 'https://flower-shop-be.vercel.app';
