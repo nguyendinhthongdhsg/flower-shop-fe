be67e3b24afc9671efb7505d7c835bf72fa061dd
Nguyen Dinh Thong <nguyenthong244466666@gmail.com>
2023-07-07 16:13:19 +0700

    setup content

--- src/Components/Content/ContentDirectory/index.tsx
+++ src/Components/Content/ContentDirectory/index.tsx
@@ -4,6 +4,7 @@ import styles from './ContenDirectory.module.scss';
 import { TypeDirectory, TypeFlower } from '@/Types';
 import Heading from '@/Components/Heading';
 import Card from '@/Components/Card';
+import { FaListUl } from 'react-icons/fa';
 
 const cx = classNames.bind(styles);
 
@@ -18,12 +19,12 @@ const ContentDirectory: React.FC<ContenDirectoryProps> = async ({ directory }) =
             <div className={cx('content')}>
                 {listFlower && (
                     <>
-                        <Heading title={directory.name ? directory.name : ''} />
+                        <Heading title={directory.name ? directory.name : ''} icon={<FaListUl />} />
                         <ul className={cx('list')}>
                             {listFlower[0] &&
                                 listFlower.map((item: TypeFlower, index: number) => {
                                     return (
-                                        <li key={index}>
+                                        <li key={index} className={cx('item')}>
                                             <Card flower={item} />
                                         </li>
                                     );
--- src/Components/Heading/index.tsx
+++ src/Components/Heading/index.tsx
@@ -2,6 +2,7 @@
 
 import classNames from 'classnames/bind';
 import styles from './Heading.module.scss';
+import { IconType } from 'react-icons';
 
 const cx = classNames.bind(styles);
 
@@ -9,13 +10,17 @@ interface HeadingProps {
     title: string;
     subtitle?: string;
     center?: boolean;
+    icon?: JSX.Element;
 }
 
-const Heading: React.FC<HeadingProps> = ({ title, subtitle, center }) => {
+const Heading: React.FC<HeadingProps> = ({ title, subtitle, center, icon }) => {
     return (
         <div className={cx('wrapper')}>
             <div className={cx('content')}>
-                <div className={cx('heading', center && 'center')}>{title}</div>
+                <div className={cx('heading', center && 'center')}>
+                    {icon ? <div className={cx('heading-icon')}>{icon}</div> : ''}
+                    {title}
+                </div>
                 <div className={cx('heading-sub', center && 'center')}>{subtitle}</div>
             </div>
         </div>
